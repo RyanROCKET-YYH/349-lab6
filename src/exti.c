16f05e49fb1ea63d00a31fb925b99503ea0ac670
Yuhong Yao <yuhongy@andrew.cmu.edu>
2024-04-10 21:02:34 -0400

    not working

--- src/exti.c
+++ src/exti.c
@@ -11,6 +11,7 @@
 #include <exti.h>
 #include <gpio.h>
 #include <rcc.h>
+#include <nvic.h>
 
 #define UNUSED __attribute__((unused))
 
@@ -19,6 +20,14 @@
 #define SYSCFG_BASE (struct syscfg_reg_map *) (0x40013800) 
 #define EXTI_BASE   (struct exti_reg_map *)   (0x40013C00) 
 #define EXTI_PR13   (1 << 13)
+
+#define EXTI0_INT_NUM (22)
+#define EXTI1_INT_NUM (23)
+#define EXTI2_INT_NUM (24)
+#define EXTI3_INT_NUM (25)
+#define EXTI4_INT_NUM (26)
+#define EXTI9_5_INT_NUM (39)
+#define EXTI15_10_INT_NUM (56)
 
 volatile uint8_t exti_led = 0;
 
@@ -52,8 +61,42 @@ void enable_exti(UNUSED gpio_port port, UNUSED uint32_t channel, UNUSED uint32_t
     rcc->apb2_enr |= RCC_SYSCFGEN;
 
     struct syscfg_reg_map* syscfg = SYSCFG_BASE;
-    uint32_t exticr_x = channel / 4;
+    uint32_t exticr_x = (channel / 4);
     uint32_t exti_x = (channel % 4) * 4;
+    switch (channel) {
+        case 0:
+            nvic_irq(EXTI0_INT_NUM, IRQ_ENABLE);
+            break;
+        case 1:
+            nvic_irq(EXTI1_INT_NUM, IRQ_ENABLE);
+            break;
+        case 2:
+            nvic_irq(EXTI2_INT_NUM, IRQ_ENABLE);
+            break;
+        case 3:
+            nvic_irq(EXTI3_INT_NUM, IRQ_ENABLE);
+            break;
+        case 4:
+            nvic_irq(EXTI4_INT_NUM, IRQ_ENABLE);
+            break;
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            nvic_irq(EXTI9_5_INT_NUM, IRQ_ENABLE);
+            break;
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            nvic_irq(EXTI15_10_INT_NUM, IRQ_ENABLE);
+            break;
+        default:
+            break;
+    }
 
     syscfg->exticr[exticr_x] &= ~(0xF << exti_x);
     syscfg->exticr[exticr_x] |= (port << exti_x);
@@ -88,11 +131,11 @@ void exti_clear_pending_bit(UNUSED uint32_t channel) {
 void EXTI13_IRQHandler(void) {
     struct exti_reg_map* exti = EXTI_BASE;
     if (exti->pr & EXTI_PR13) { 
-        exti->pr = EXTI_PR13;
+
         if (exti_led) {
-            gpio_clr(GPIO_A, 5);
+            gpio_clr(GPIO_A, 4);
         } else {
-            gpio_set(GPIO_A, 5);
+            gpio_set(GPIO_A, 4);
         }
 
     exti_led = !exti_led;
--- src/main.c
+++ src/main.c
@@ -25,6 +25,7 @@
 #include <i2c.h>
 #include <arm.h>
 #include <timer.h>
+#include <exti.h>
 
 // void vTimerTask(void *pvParameters) {
 //     (void)pvParameters;
@@ -39,10 +40,12 @@ int main( void ) {
     i2c_master_init(80);
     timer_init(3, 100, 16);
     gpio_init(GPIO_B, 4, MODE_ALT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT2);
-    
-    timer_start_pwm(3, 1, 100, 16, 2);
+    gpio_init(GPIO_A, 4, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_NONE, ALT0);
+    // timer_start_pwm(3, 1, 100, 16, 2);
     // timer_set_duty_cycle(3,1, 16);
     gpio_init(GPIO_C, 13, MODE_INPUT, OUTPUT_PUSH_PULL, OUTPUT_SPEED_LOW, PUPD_PULL_UP, ALT0);
+    enable_exti(GPIO_C, 13, RISING_FALLING_EDGE);
+    
     for (;;) {}
     return 0;
 }

f46db871ee488c5e632b157ac3ee50e56454f551
Yi Ying Li <yiyinl4@andrew.cmu.edu>
2024-04-18 16:41:57 -0400

    update gpio

--- include/gpio_pin_yiying.h
+++ include/gpio_pin_yiying.h
@@ -37,7 +37,7 @@
 
 #define SERVO_PORT       3  // D4
 #define SERVO_PIN        5 
-#define GPIO_JP_PORT     1  //TODO: connect by myself
+#define GPIO_JP_PORT     0  // Green LED: PA_10(0, 10)
 #define GPIO_JP_PIN      10
 #define MOTOR_EN_PORT    1  // D5
 #define MORTO_EN_PIN     4 
@@ -45,7 +45,7 @@
 #define ENC_B_PIN        8
 #define ENC_A_PORT       0  // D8
 #define ENC_A_PIN        9
-#define BUTTON1_PORT     1  // A3(PB0)
+#define BUTTON1_PORT     1  // A3(PB0) // BUTTON for exti
 #define BUTTON1_PIN      0
 #define BUTTON2_PORT     1  // //reset???
 #define BUTTON2_PIN      6
@@ -53,7 +53,7 @@
 #define MORTO_IN1_PIN    7
 #define MORTO_IN2_PORT   0  // D12
 #define MORTO_IN2_PIN    6
-#define LEDG_PORT        2  // A4(PC_1) //red led
+#define LEDG_PORT        2  // A4(PC_1) //Red LED: (2_1)
 #define LEDG_PIN         1
 
 #endif  // _GPIO_PIN_YIYING_H_
